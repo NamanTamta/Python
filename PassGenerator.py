476eb9ea4b88ff66ed1b23d85bc5268deb52422f
Naman Tamta <namantamta987@gmail.com>
2025-11-09 20:11:52 +0530

    Random Pass Generator

--- PassGenerator.py
+++ PassGenerator.py
@@ -1,12 +1,21 @@
 import random
 
+# Define character sets
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
 symbols = "!@#$%^&*()_+{}|:<>?-=[];',./`~"
+
+# Combine all character sets
 allchar = upper + lower + numbers + symbols
+
+# Get desired password length from user
 length = int(input("Enter the length of the password: "))
+
+# Generate password
 password = " "
+
+# Loop to create password of specified length
 for a in range(length):
     password += random.choice(allchar)
 print("Your generated password is: ", password)
